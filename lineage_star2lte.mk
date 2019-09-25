34cd8340d98f7cca051052011916a8f7140717f7
AzzyC <azmath2000@gmail.com>
2019-09-26 00:12:32 +0100

    star2lte: cts: fingerprint override

--- lineage_star2lte.mk
+++ lineage_star2lte.mk
@@ -7,3 +7,8 @@ $(call inherit-product, device/samsung/star2lte/full_star2lte.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_star2lte
+
+BUILD_FINGERPRINT := samsung/star2ltexx/starlte:9/PPR1.180610.011/G965FXXS4CSDD:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        PRIVATE_BUILD_DESC="star2ltexx-user 9 PPR1.180610.011 G965FXXS4CSDD release-keys"
